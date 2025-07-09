c2f0a074aa1d64edcc1f2b268240287c92a51742
Jae-Joon Lee <lee.j.joon@gmail.com>
2025-07-09 20:00:07 +0900

    feat: Initial project setup with Poetry
    - Initialize project with poetry
    - Add runtime dependencies: pydantic_ai, matplotlib, numpy, pandas
    - Add test dependency: seaborn
    - Create package structure `mpl_llm_selector`
    - Move initial python files to the package directory
    - Configure pyproject.toml to specify the package directory

---
+++ mpl_llm_selector/mpl_plot_properties.py
@@ -0,0 +1,194 @@
+import pandas as pd
+from matplotlib.colors import to_hex
+
+
+# class MplContextHelper:
+class MplAxesProperties:
+    def __init__(self, ax):
+        self._ax = ax
+
+        # self._artist_attrs = self.get_artist_attrs_all()
+        xx = self.get_artist_attrs_all(get_id=True)
+        self._df_artists = pd.DataFrame(xx)
+
+        labs = self.get_label_properties()
+        self._df_legend = pd.DataFrame(labs)
+
+    def get_artist_attrs(self, l, get_bbox=True, get_id=True):
+        ax = self._ax
+
+        if get_bbox:
+            bbox = l.get_window_extent().transformed(ax.transData.inverted())
+            x = dict(zip(["xmin", "ymin", "xmax", "ymax"], bbox.extents))
+        else:
+            x = dict()
+
+        # x["kind"] = kind
+
+        for k in ["color", "facecolor", "edgecolor", "linewidth", "label"]:
+            if hasattr(l, f"get_{k}"):
+                v = getattr(l, f"get_{k}")()
+                if "color" in k:
+                    v = v and to_hex(v)
+                x[k] = v
+            else:
+                x[k] = None
+
+        if get_id:
+            x["id"] = id(l)
+
+        return x
+
+
+    def get_artist_attrs_all(self, get_id=True):
+        ax = self._ax
+        xx = []
+        for kind, artists in [("lines", ax.lines),
+                              ("patches", ax.patches),
+                              ("collections", ax.collections)]:
+            for i, l in enumerate(artists):
+
+                x = self.get_artist_attrs(l, get_id=get_id)
+                x["selector"] = f"{kind}[{i}]"
+                xx.append(x)
+
+        return xx
+
+    def get_label_properties(self):
+
+        ax = self._ax
+
+        labs = []
+        for h, t in zip(ax.legend_.legend_handles, ax.legend_.get_texts()):
+            lab = dict(label=t.get_text())
+            lab.update(self.get_artist_attrs(h, get_bbox=False, get_id=False))
+            labs.append(lab)
+
+        return labs
+
+    def get_axes_properties(self):
+
+        ax = self._ax
+        df_legend = self._df_legend
+        df_artists = self._df_artists
+
+        xticklabels = pd.DataFrame([dict(x=t._x, value=t.get_text())
+                                    for t in ax.get_xticklabels()])
+        yticklabels = pd.DataFrame([dict(x=t._y, value=t.get_text())
+                                    for t in ax.get_yticklabels()])
+
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+
+        return dict(xlim=xlim,
+                    ylim=ylim,
+                    xticklabels_dataframe=xticklabels,
+                    yticklabels_dataframe=yticklabels,
+                    artist_dataframe=df_artists,
+                    legend_dataframe=df_legend
+                    )
+
+    def get_context(self):
+
+        prop = self.get_axes_properties()
+        context = f"""
+
+## Data limits of the axes
+
+x = {prop["xlim"]}
+y = {prop["ylim"]}
+
+## Properties of Artists
+
+{prop["artist_dataframe"].to_markdown(index=False)}
+
+## X-axis tick labeles and their location
+
+{prop["xticklabels_dataframe"].to_markdown(index=False)}
+
+
+## y-axis tick labeles and their location
+
+{prop["yticklabels_dataframe"].to_markdown(index=False)}
+
+## legend
+
+{prop["legend_dataframe"].to_markdown(index=False)}
+
+"""
+
+        return context
+
+    def select_artists(self, id_list):
+        selectors = self._df_artists.set_index("id").loc[id_list]["selector"]
+        selected = SelectedArtists(self._ax, self._df_artists.set_index("selector"),
+                                   selectors)
+        return selected
+
+import re
+p_selector = re.compile(r"(\w+)\[(\d+)\]")
+
+class SelectedArtists:
+    # @classmethod
+    # def from_axes(cls, ax):
+    #     pass
+
+    @classmethod
+    def from_selectors(cls, ax, selectors):
+        ax_prop = MplAxesProperties(ax)
+        return cls(ax, ax_prop._df_artists.set_index("selector"), selectors)
+
+    def __init__(self, ax, df_artists, selectors):
+        self._ax = ax
+        self._df_artists_parent = df_artists
+        self._selected_df = df_artists.loc[selectors]
+        self._selectors = selectors
+
+    def inverted(self):
+        inverted_idx = self._df_artists_parent.index.difference(self._selectors)
+
+        return type(self)(self._ax, self._df_artists_parent, inverted_idx)
+
+    def iter_artists(self):
+        for selector in self._selectors:
+            m = p_selector.match(selector)
+            kind, ind = m.groups()
+            a = getattr(self._ax, kind)[int(ind)]
+            yield a
+
+    def show_selectors(self):
+        return list(self._selectors)
+
+    def set(self, *kl, **kwargs):
+        for a in self.iter_artists():
+            a.set(*kl, **kwargs)
+
+if False:
+    helper = MplAxesProperties(ax)
+    c = helper.get_context()
+
+# print(df_artists.to_markdown())
+
+# %%
+
+
+# %%
+
+
+# %%
+
+
+# print(context)
+
+
+# if False:
+#     from matplotlib.patches import Rectangle
+#     xx = []
+#     for kind, artists in [("lines", ax.lines),
+#                           ("patches", ax.patches),
+#                           ("collections", ax.collections)]:
+#         for i, l in enumerate(artists):
+
+#             x = get_artist_attrs(l, ax)
+#             x["accessor"] = f"{kind}[{i}]"
+#             xx.append(x)
